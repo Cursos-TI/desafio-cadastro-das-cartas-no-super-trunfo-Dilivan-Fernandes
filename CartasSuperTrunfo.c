7ae4d6944a921202846c19cf55728a84f8781510
Dilivan Fernandes <dilivanr7@gmail.com>
2025-01-30 09:19:20 -0300

    Editei a interface

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -45,59 +45,59 @@ int main() {
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf(".........................SUPER TRUNFO.............................\n\n");// Texto da interface.
-printf("//////////////////CADASTRE A 1ª CARTA/////////////////////////////\n\n");// Texto da interface.
+printf("CADASTRE A PRIMEIRA CARTA\n\n");// Texto da interface.
 
 struct Carta1 carta1;//  Estrutura de dados da primeira carta
 struct Carta2 carta2; //  Estrutura de dados da segunda carta.
 
-//  Captura de dados da primeira carta.
-   printf("Digite o estado...............: ");
-   scanf("%s",&carta1.Estado);// Captura do estado.
+        //  Captura de dados da primeira carta.
+        printf("Digite o estado...............: ");
+        scanf("%s",&carta1.Estado);// Captura do estado.
 
-   printf("Digite o código da carta......: ");
-   scanf("%s",&carta1.CodigoDaCarta);// Captura do código da carta.
+        printf("Digite o código da carta......: ");
+        scanf("%s",&carta1.CodigoDaCarta);// Captura do código da carta.
 
-   printf("Digite o nome da cidade.......: ");
-   scanf("%s",&carta1.NomeDaCidade);// Captura do nome da cidade.
+        printf("Digite o nome da cidade.......: ");
+        scanf("%s",&carta1.NomeDaCidade);// Captura do nome da cidade.
 
-   printf("Digite a população............: ");
-   scanf("%d",&carta1.Populacao);// Captura da população.
+        printf("Digite a população............: ");
+        scanf("%d",&carta1.Populacao);// Captura da população.
 
-   printf("Digite a área em km²..........: ");
-   scanf("%f",&carta1.Area);// Captura da área.
+        printf("Digite a área em km²..........: ");
+        scanf("%f",&carta1.Area);// Captura da área.
 
-   printf("Digite o PIB .................: ");
-   scanf("%f",&carta1.PIB);// Captura do PIB.
+        printf("Digite o PIB .................: ");
+        scanf("%f",&carta1.PIB);// Captura do PIB.
 
-   printf("Digite a quantidade de pontos turísticos..: ");
-   scanf("%d",&carta1.PontosTuristicos);// Captura da quantidade de pontos turísticos.
+        printf("Digite a quantidade de pontos turísticos..: ");
+        scanf("%d",&carta1.PontosTuristicos);// Captura da quantidade de pontos turísticos.
 
- printf("\n\n//////////////////CADASTRE A 2ª CARTA/////////////////////////////\n\n");// Texto da interface.
+ printf("\n\nCADASTRE A SEGUNDA CARTA\n\n");// Texto da interface.
 
 
-    //  Captura de dados da segunda carta.
-   printf("Digite o estado...............: ");
-   scanf("%s",&carta2.Estado);// Captura do estado.
+      //  Captura de dados da segunda carta.
+        printf("Digite o estado...............: ");
+        scanf("%s",&carta2.Estado);// Captura do estado.
 
-   printf("Digite o código da carta......: ");
-   scanf("%s",&carta2.CodigoDaCarta);// Captura do código da carta.
+        printf("Digite o código da carta......: ");
+        scanf("%s",&carta2.CodigoDaCarta);// Captura do código da carta.
 
-   printf("Digite o nome da cidade.......: ");
-   scanf("%s",&carta2.NomeDaCidade);// Captura do nome da cidade.
+        printf("Digite o nome da cidade.......: ");
+        scanf("%s",&carta2.NomeDaCidade);// Captura do nome da cidade.
 
-   printf("Digite a população............: ");
-   scanf("%d",&carta2.Populacao);// Captura da população.
+        printf("Digite a população............: ");
+        scanf("%d",&carta2.Populacao);// Captura da população.
 
-   printf("Digite a área em km²..........: ");
-   scanf("%f",&carta2.Area);// Captura da área.
+        printf("Digite a área em km²..........: ");
+        scanf("%f",&carta2.Area);// Captura da área.
 
-   printf("Digite o PIB .................: ");
-   scanf("%f",&carta2.PIB);// Captura do PIB.
+        printf("Digite o PIB .................: ");
+        scanf("%f",&carta2.PIB);// Captura do PIB.
 
-   printf("Digite a quantidade de pontos turísticos..: ");
-   scanf("%d",&carta2.PontosTuristicos);// Captura da quantidade de pontos turísticos.
+        printf("Digite a quantidade de pontos turísticos..: ");
+        scanf("%d",&carta2.PontosTuristicos);// Captura da quantidade de pontos turísticos.
 
-   printf("\n\n");//  Quebra de linhas.
+ printf("\n\n");//  Quebra de linhas.
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
@@ -105,26 +105,25 @@ struct Carta2 carta2; //  Estrutura de dados da segunda carta.
 
  printf("Cartas cadastradas\n\n");
 
- //  Dados da primeira carta.
-printf("Estado: %s\n",carta1.Estado);
-printf("Código da carta: %s\n",carta1.CodigoDaCarta);
-printf("Nome da cidade: %s\n",carta1.NomeDaCidade);
-printf("População: %d \n",carta1.Populacao);
-printf("Área: %.2f\n",carta1.Area);
-printf("PIB: %.2f\n",carta1.PIB);
-printf("Pontos turísticos: %d\n",carta1.PontosTuristicos);
+   // Dados da primeira carta.
+    printf("Estado: %s\n",carta1.Estado);
+    printf("Código da carta: %s\n",carta1.CodigoDaCarta);
+    printf("Nome da cidade: %s\n",carta1.NomeDaCidade);
+    printf("População: %d \n",carta1.Populacao);
+    printf("Área: %.2f\n",carta1.Area);
+    printf("PIB: %.2f\n",carta1.PIB);
+    printf("Pontos turísticos: %d\n",carta1.PontosTuristicos);
 
 printf("\n\n");//  Quebra de linhas.
 
- //  Dados da segunda carta.
-
-printf("Estado: %s\n",carta2.Estado);
-printf("Código da carta: %s\n",carta2.CodigoDaCarta);
-printf("Nome da cidade: %s\n",carta2.NomeDaCidade);
-printf("População: %d\n",carta2.Populacao);
-printf("Área: %.2f\n",carta2.Area);
-printf("PIB: %.2f \n",carta2.PIB);
-printf("Pontos turísticos: %d\n",carta2.PontosTuristicos);
+   //  Dados da segunda carta.
+    printf("Estado: %s\n",carta2.Estado);
+    printf("Código da carta: %s\n",carta2.CodigoDaCarta);
+    printf("Nome da cidade: %s\n",carta2.NomeDaCidade);
+    printf("População: %d\n",carta2.Populacao);
+    printf("Área: %.2f\n",carta2.Area);
+    printf("PIB: %.2f \n",carta2.PIB);
+    printf("Pontos turísticos: %d\n",carta2.PontosTuristicos);
 
 printf("\n\n");//  Quebra de linhas.
 
